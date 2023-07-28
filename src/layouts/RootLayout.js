45fca35e8b3fee31325e6bac67b4605bf9efa3bd
nacho131994 <nachommunoz13@gmail.com>
2023-07-28 20:29:33 +0200

    Fixed some bugs

--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -8,6 +8,7 @@ const RootLayout = () => {
         <header>
           <nav>
             <h1>IngredientsCalculator</h1>
+            {/* <img src="images\branding.jpg" /> */}
 
             <NavLink to="/">
               <p className="navButtons">HOME</p>
